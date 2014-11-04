7173bdb8472c90cd37e272c9910c0161c63a9263
villa7 <villa7.kluge5@gmail.com>
2014-11-04 15:49:50 -0500

    add smrt autonomous

--- GlobalFunctions/debug.h
+++ GlobalFunctions/debug.h
@@ -3,12 +3,18 @@ void debug_l(const string str);
 void debug_clear();
 
 void debug_l(const string str) {
+	if (!defined(inDebug)) {
+		inDebug = true;
+	}
 	if (inDebug) {
 		writeDebugStream(str);
 	}
 }
 
 void debug(const string str) {
+	if (!defined(inDebug)) {
+		inDebug = true;
+	}
 	if (inDebug) {
 		writeDebugStreamLine(str);
 	}
