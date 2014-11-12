b83fcf667e0fa79e681e67ecfdb1df301692e544
villa7 <villa7.kluge5@gmail.com>
2014-11-12 15:49:42 -0500

    update

--- GlobalFunctions/debug.h
+++ GlobalFunctions/debug.h
@@ -1,21 +1,44 @@
-void debug(const string str);
-void debug_l(const string str);
+void debug(const string str, int val);
+void debug_l(const string str, int val);
 void debug_clear();
+string msg;
 
-void debug_l(const string str) {
+void debug_l(const string str, int val) {
 	if (!defined(inDebug)) {
 		inDebug = true;
 	}
 	if (inDebug) {
+		StringFormat(msg, str, val);
+		writeDebugStream(msg);
+	}
+}
+
+void debug_l(const string str, string val) {
+	if (!defined(inDebug)) {
+		inDebug = true;
+	}
+	if (inDebug) {
+		strcat(str, val);
 		writeDebugStream(str);
 	}
 }
 
-void debug(const string str) {
+void debug(const string str, int val) {
 	if (!defined(inDebug)) {
 		inDebug = true;
 	}
 	if (inDebug) {
+		StringFormat(msg, str, val);
+		writeDebugStreamLine(msg);
+	}
+}
+
+void debug(const string str, string val) {
+	if (!defined(inDebug)) {
+		inDebug = true;
+	}
+	if (inDebug) {
+		strcat(str, val);
 		writeDebugStreamLine(str);
 	}
 }
